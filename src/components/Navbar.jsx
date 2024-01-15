0b85f9344b61698c3490cac39eca6c26e92a6062
Blitu82 <garpaues@yahoo.es>
2024-01-15 19:50:41 +0000

    image in public folder

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Image, Stack } from '@chakra-ui/react';
 function Navbar() {
   return (
     <Stack direction="row" as="nav" bg="#222" p="15px">
-      <Image boxSize="40px" src="public\logo_example.png" alt="logo"></Image>
+      <Image boxSize="40px" src="/logo_example.png" alt="logo"></Image>
       <Heading as="h1" color="white">
         TrekTrackr
       </Heading>
