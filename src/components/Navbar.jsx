92dbe77b3e72d5a90358071cf656ed9b957402c4
Blitu82 <garpaues@yahoo.es>
2024-01-15 19:51:50 +0000

    image fix 2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import { Heading } from '@chakra-ui/react';
 import { Image, Stack } from '@chakra-ui/react';
+import imgUrl from '../assets/logo_example.png';
 
 function Navbar() {
   return (
     <Stack direction="row" as="nav" bg="#222" p="15px">
-      <Image boxSize="40px" src="/logo_example.png" alt="logo"></Image>
+      <Image boxSize="40px" src={imgUrl} alt="logo"></Image>
       <Heading as="h1" color="white">
         TrekTrackr
       </Heading>
