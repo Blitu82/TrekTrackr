548b83d1b7216369a7f5778efa6fcd39b29da98a
estanislaumendes <estanislau.mendes@hotmail.com>
2024-01-17 19:43:13 +0000

    navbar about button and dark mode button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,32 @@
-import { Heading } from '@chakra-ui/react';
+import { Flex, Heading, Switch } from '@chakra-ui/react';
 import { Image, Stack } from '@chakra-ui/react';
 import imgUrl from '../assets/logo_example.png';
+import { Button } from '@chakra-ui/react';
 
 function Navbar() {
   return (
-    <Stack direction="row" as="nav" bg="#222" p="15px">
-      <Image boxSize="40px" src={imgUrl} alt="logo"></Image>
-      <Heading as="h1" color="white">
-        TrekTrackr
-      </Heading>
-    </Stack>
+    <Flex
+      direction="row"
+      as="nav"
+      align="center"
+      justify="space-between"
+      bg="#222"
+      p="15px"
+    >
+      <Stack direction="row">
+        <Image boxSize="40px" src={imgUrl} alt="logo"></Image>
+        <Heading as="h1" color="white">
+          TrekTrackr
+        </Heading>
+      </Stack>
+
+      <Stack direction="row" align="center" spacing="20px">
+        <Button colorScheme="yellow" size="lg">
+          About
+        </Button>
+        <Switch colorScheme="yellow" size="lg"></Switch>
+      </Stack>
+    </Flex>
   );
 }
 
